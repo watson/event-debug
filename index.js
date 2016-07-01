3ddbf4846a80c9b437b50f3fa65fa5a8d02e9c7e
Thomas Watson Steen <w@tson.dk>
2016-07-01 21:29:31 +0200

    Abort if emit is not a function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 module.exports = function (emitter, type) {
   var emit = emitter && emitter.emit
-  if (!emit) return
+  if (typeof emit !== 'function') return
   emitter.emit = function (event) {
     console.log('%s: %s', type || emitter.constructor.name, event)
     return emit.apply(this, arguments)
